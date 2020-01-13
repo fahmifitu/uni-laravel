1b8ecc4854db5feda5e88064d09ff2499f603ba2
Fahmi Elfituri <fahmi.fitu@gmail.com>
2020-01-13 14:26:33 +0200

    commit message here

---
+++ src/UniLaravelServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace fahmifitu\UniLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class UniLaravelServiceProvider extends ServiceProvider
+{
+
+	public function boot()
+	{
+		dd('rfe');
+	}
+
+	public function register()
+	{
+
+	}
+}
