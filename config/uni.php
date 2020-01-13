b39b38be358b2d431f3e26632ad722a56db72cf5
Fahmi Elfituri <fahmi.fitu@gmail.com>
2020-01-13 22:49:24 +0200

    dist functionality

--- config/uni.php
+++ config/uni.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-	'url' => env('UNI_API_URL','http://uni-test.miza.ly')
+	'url' => env('UNI_API_URL', 'http://uni-test.miza.ly'),
+	'distributor_email' => env('UNI_DIST_EMAIL', false),
+	'distributor_password' => env('UNI_DIST_PASSWORD', false)
 ];
--- src/Uni.php
+++ src/Uni.php
@@ -8,9 +8,16 @@ use GuzzleHttp\Exception\GuzzleException;
 class Uni
 {
 	private $client;
+	private $distEmail;
+	private $distPassword;
 
 	public function __construct()
 	{
+		if (! config('uni.distributor_email') )
+			$this->$distEmail = config('uni.distributor_email');
+        if (! config('uni.distributor_password') )
+        	$this->distPassword = config('uni.distributor_password');
+
 		$this->client = new Client([
 			'base_uri' => config('uni.url'),
             'headers' => [
@@ -20,7 +27,18 @@ class Uni
             'http_errors' => false,
 		]);
 	}
-
+	public function generateVoucher(string $email, string $password, string $phone, float $amount, int $number = 1)
+	{
+		$data = [
+		  	'IdDist' => $email,
+		  	'DistPassword' => $password,
+		  	'PhoneNumber' => $phone,
+		  	'NumberOfVoucher' => $number,
+		 	'AmountOfRecharges' => $amount,
+			'VoucherType' => 1,
+		];
+		$this->request('POST', '/BuyRechargeCode', $data);
+	}
 	public function consumeVoucher(string $email, string $password, string $voucherNumber,string $pin, float $amount)
 	{
 		$data = [
@@ -31,21 +49,8 @@ class Uni
 			'AmountToConsume' => $amount,
 			'codePin' => $pin
 		];
-		try {
-			$res = $this->client->request('POST', '/TopUpRequest', [
-				'json' => $data
-			]);
-			return response()->json([
-				'code' => $res->getStatusCode(),
-				'message' => $res->getReasonPhrase()
-			]);
-		} catch (GuzzleException $e) {
-			return response()->json([
-				'error' => $e->getMessage()
-			], 500);
-		}
+		$this->request('POST', '/TopUpRequest', $data);
 	}
-
 	public function sendPincode(string $email, string $password, string $voucherNumber)
 	{
 		$data = [
@@ -53,19 +58,31 @@ class Uni
 			'userPassword' => $password,
 			'voucherNumber' => $voucherNumber
 		];
-		try {
-			$res = $this->client->request('POST', '/sendSecret', [
-				'json' => $data
-			]);
-			return response()->json([
-				'code' => $res->getStatusCode(),
-				'message' => $res->getReasonPhrase()
-			]);
-		} catch (GuzzleException $e) {
-			return response()->json([
-				'error' => $e->getMessage()
-			], 500);
-		}
+		$this->request('POST', '/sendSecret', $data);
 	}
+	private function request($verb, $uri, array $payload = [])
+    {
+        $response = $this->client->request($verb, $uri,
+            empty($payload) ? [] : ['form_params' => $payload]
+        );
+        if ($response->getStatusCode() != 200) {
+            return $this->handleRequestError($response);
+        }
+        $responseBody = (string) $response->getBody();
+        return json_decode($responseBody, true) ?: $responseBody;
+    }
 
+    private function handleRequestError(ResponseInterface $response)
+    {
+        if ($response->getStatusCode() == 422) {
+            throw new ValidationException(json_decode((string) $response->getBody(), true));
+        }
+        if ($response->getStatusCode() == 404) {
+            throw new NotFoundException();
+        }
+        if ($response->getStatusCode() == 400) {
+            throw new FailedActionException((string) $response->getBody());
+        }
+        throw new \Exception((string) $response->getBody());
+    }
 }
